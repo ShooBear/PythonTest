7bb0305a933eccaabaea9aa676edb67f0581b8ae
조기영 <ky.cho@piolink.com>
2018-11-05 18:57:45 +0900

    Merge sort에 비교 횟수를 기록할 수 있도록 변경

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -3,33 +3,48 @@
 
 def merge(left, right):
     #array to store the sorted list
-    sorted = []
+    sorted_list = []
     i, j = 0, 0
     while i < len(left) and j < len(right):
+        merge_sort.count += 1
         if left[i] < right[j]:
-            sorted.append(left[i])
+            sorted_list.append(left[i])
             i += 1
         else:
-            sorted.append(right[j])
+            sorted_list.append(right[j])
             j += 1
 
-    sorted += left[i:]
-    sorted += right[j:]
-    return sorted
+    sorted_list += left[i:]
+    sorted_list += right[j:]
+    return sorted_list
 
 
-def merge_sort(li):
+def _merge_sort(li):
     "function to compute merge-sort"
     if len(li) == 1:
         return li
 
     middle = len(li) / 2
-    left_li = merge_sort(li[:middle])
-    right_li = merge_sort(li[middle:])
-    return merge(left_li, right_li)
+    left_li = _merge_sort(li[:middle])
+    right_li = _merge_sort(li[middle:])
+    sorted_list = merge(left_li, right_li)
+    return sorted_list
+
+
+def merge_sort(A, first, last):
+    merge_sort.count = 0
+    SA = _merge_sort(A[first:last])
+    A = A[:first] + SA + A[last:]
+    return A, merge_sort.count
 
 
 if __name__ == "__main__":
-    #sample test case
-    li = [10, 5, 2, 3, 7, 4, 8, 9]
-    print(merge_sort(li))
+    A = [10, 5, 2, 3, 7, 4, 8, 9, 11, 3, 1, 555,
+         112, 31, 5, 12, 31, 51, 51, 1224, 151, 23, 12, 51, 67, 132451, 24]
+    print(A)
+    SA, compare_cnt = merge_sort(A, 0, 11)
+    print(A)
+    print("Compare Count : {}".format(compare_cnt))
+    print(SA[:5])
+    print(SA[5:11])
+    print(SA[11:])
